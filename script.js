eb0c16e686b87f4c34d0c34bdb11f2709c253310
brauliomontiel28-del <brauliomontiel28@gmail.com>
2026-07-29 21:15:05 -0600

    Remove DOMContentLoaded event listener code

--- script.js
+++ script.js
@@ -2,28 +2,6 @@
 // Baby Shower Zoe Julieth
 // Script principal
 // ===============================
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    const intro = document.getElementById("intro");
-    const hero = document.querySelector(".hero");
-
-    setTimeout(() => {
-
-        intro.style.opacity = "0";
-
-        setTimeout(() => {
-
-            intro.style.display = "none";
-
-            hero.classList.remove("oculto");
-hero.classList.add("mostrar");
-
-        }, 1200);
-
-    }, 3500);
-
-});
 // ===============================
 // Cuenta regresiva
 // ===============================
